e2d588df3dfefeaaa23c8b1730643a3f7b23e6d8
Mohamed Ben <mohamed@ipfinder.io>
2019-08-28 14:58:28 +0100

    add roxygen support

---
+++ R/ipfinder-package.R
@@ -0,0 +1,17 @@
+#' The official Lua client library for the IPFinder.io
+#'
+#' IP address details (city, region, country, postal code, latitude and more ..).
+#' ASN details (Organization name, registry,domain,comany_type, and more .. ).
+#' Firewall by supported formats details (apache_allow, nginx_deny, CIDR , and more ..).
+#' IP Address Ranges by the Organization name details (list_asn, list_prefixes , and more ..).
+#' Get Domain IP (asn, organization,country_code ....).
+#' Get Domain IP history (total_ip, list_ip,organization,asn ....).
+#' Get list Domain By ASN, Country,Ranges (select_by , total_domain , list_domain ....).
+#'
+#' @md
+#' @name ipfinder
+#' @docType package
+#' @author Mohamed Ben rebia <mohamed@ipfinder.io>
+#' @import httr
+#' @import jsonlite
+NULL
