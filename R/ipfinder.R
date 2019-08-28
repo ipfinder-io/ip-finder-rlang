6687ce0c2f0802f91506d7cf0bf25349dca25b9b
Mohamed Ben <mohamed@ipfinder.io>
2019-08-28 13:01:26 +0100

    fix comments

--- R/ipfinder.R
+++ R/ipfinder.R
@@ -2,11 +2,11 @@
 #'
 #' IP address details (city, region, country, postal code, latitude and more ..).
 #' ASN details (Organization name, registry,domain,comany_type, and more .. ).
-# Firewall by supported formats details (apache_allow, nginx_deny, CIDR , and more ..).
-# IP Address Ranges by the Organization name details (list_asn, list_prefixes , and more ..).
-# Get Domain IP (asn, organization,country_code ....).
-# Get Domain IP history (total_ip, list_ip,organization,asn ....).
-# Get list Domain By ASN, Country,Ranges (select_by , total_domain , list_domain ....). 
+#' Firewall by supported formats details (apache_allow, nginx_deny, CIDR , and more ..).
+#' IP Address Ranges by the Organization name details (list_asn, list_prefixes , and more ..).
+#' Get Domain IP (asn, organization,country_code ....).
+#' Get Domain IP history (total_ip, list_ip,organization,asn ....).
+#' Get list Domain By ASN, Country,Ranges (select_by , total_domain , list_domain ....). 
 #'
 #' @md
 #' @name ipfinder
